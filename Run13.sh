9d2afb8d57def68361101ba712931b069529e333
An Bui <91354810+AnBui2004@users.noreply.github.com>
2022-05-25 21:24:57 +0700

    Create Run13.sh

---
+++ Run13.sh
@@ -0,0 +1,23 @@
+sudo apt-get update && apt-get install qemu -y
+sudo apt install qemu-utils -y
+sudo apt install qemu-system-x86-xen -y
+sudo apt install qemu-system-x86 -y
+qemu-img create -f raw windows7nano.img 64G
+wget -O RTL8139F.iso 'https://drive.google.com/uc?export=download&id=1wDL8vo9mmYKw1HKXZzaYHoKmzSt_wXai'
+wget -O windows7_super-nano_lite.iso 'https://download1488.mediafire.com/h9bn32erf0tg/land087j09xszt0/Windows7_super-nano_lite.iso'
+
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+sudo qemu-system-x86_64 \
+  -m 8G \
+  -cpu EPYC \
+  -boot order=d \
+  -drive file=windows7_super-nano_lite.iso,media=cdrom \
+  -drive file=windows7nano.img,format=raw \
+  -drive file=RTL8139F.iso,media=cdrom \
+  -device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
+  -device usb-tablet \
+  -vnc :0 \
+  -cpu core2duo \
+  -smp cores=2 \
+  -device rtl8139,netdev=n0 -netdev user,id=n0 \
+  -accel tcg,thread=multi \
