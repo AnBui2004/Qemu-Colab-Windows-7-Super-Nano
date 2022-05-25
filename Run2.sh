d507c34af27cd5f2f3281d87e1cfa614d1e11749
An Bui <91354810+AnBui2004@users.noreply.github.com>
2022-05-25 18:56:01 +0700

    Update Run2.sh

--- Run2.sh
+++ Run2.sh
@@ -3,8 +3,8 @@ sudo apt install qemu-utils -y
 sudo apt install qemu-system-x86-xen -y
 sudo apt install qemu-system-x86 -y
 qemu-img create -f raw windows7nano.img 64G
-wget -O RTL8139F.iso 'https://zoydyw.bn.files.1drv.com/y4mQQI3UjoImJ_tKf4sMyOzxK84q-gSggnjanUix8yyKEm8i8o7olu8_Pa0ErbpE_U29DvLyMiuvRx1zWAxdbWlGQP11_hQdTYuXnRZjq8O29Jx_OeVvvn_TneuwXREVJRGF5vxmGRP6OMHcaHu1MM8_PURTgIRd_9IQnOudymE5us0rK6zKa6bz6Q2O35QnV_ItSajaGUvu_sCu2OBIAKsbw'
-wget -O windows7_super-nano_lite.iso 'https://afuqbq.bn.files.1drv.com/y4myuZ3xUSxcdP1JCTnDlbtjdxk6LXtD7UKbYtTXq5JD3zmv7zP5a9sLbpVv7tb0qV2j5iFbHULsSQDbWWsYtIw2y-2sb_P_tfiCP6T0D7_Si-c0bT9Dza-9GJ_o-OnK-sYZIhE_X83w09gOBTiVqnX6xitny_r6nJ27SqDVGh1-8QhLL9-HCKQVJKuHf3lPIfrjiort_xkjzeJQIHPfN1-oQ'
+wget -O RTL8139F.iso 'https://drive.google.com/uc?export=download&id=1wDL8vo9mmYKw1HKXZzaYHoKmzSt_wXai'
+wget -O windows7_super-nano_lite.iso 'https://drive.google.com/uc?export=download&id=1y5BYrgdUIV_6D5OoILUAfvYQJMGUlPmp'
 
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 sudo qemu-system-x86_64 \
